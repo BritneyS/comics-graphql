f24a5d170f34fa8c77a4714f234869f753b829e5
Britney Smith <brit.smith7@gmail.com>
2019-06-03 15:48:32 -0400

    Schema and resolvers for queries

--- src/index.js
+++ src/index.js
@@ -2,11 +2,55 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import fetch from 'node-fetch';
 
 const app = express();
 const ENV_PORT = process.env.ENV_PORT;
+const allCharactersEndpoint = "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json";
 
 app.use(cors());
+
+const schema = gql`
+    type Character {
+        id: Int
+        name: String
+        fullName: String
+        placeOfBirth: String
+        race: String
+        alignment: String
+        publisher: String
+        firstAppearance: String
+        images: ImageSet
+    }
+
+    type ImageSet {
+        xs: String
+        md: String
+    }
+
+    type Query {
+        characters: [Character]
+    }
+`;
+const resolvers = {
+    Query: {
+        characters: async () => {
+            return fetch(allCharactersEndpoint)
+            .then(res => res.json());
+        }
+    },
+    Character: {
+        race: (character) => {
+            return character.appearance.race;
+        },
+        fullName: (character) => {
+            return character.biography.fullName;
+        },
+        images: (character) => {
+            return character.images;
+        }
+    }
+};
 
 const server = new ApolloServer({
     typeDefs: schema,
@@ -18,10 +62,3 @@ server.applyMiddleware({ app, path: '/graphql' });
 app.listen({ port: ENV_PORT }, () => {
     console.log(`Apollo Server listening on http://localhost:${ENV_PORT}/graphql`);
 });
-
-const schema = gql`
-
-`;
-const resolvers = {
-
-};

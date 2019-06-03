6d7945b00bf5827057ca0648fbd9e874a4936ee0
Britney Smith <brit.smith7@gmail.com>
2019-06-03 13:42:42 -0400

    Start graphql implementation with Apollo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
 const app = express();
 const ENV_PORT = process.env.ENV_PORT;
 
 app.use(cors());
 
-app.listen({ port: ENV_PORT }, () => {
-    console.log(`Server listening on http://localhost:${ENV_PORT}`);
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
 });
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: ENV_PORT }, () => {
+    console.log(`Apollo Server listening on http://localhost:${ENV_PORT}/graphql`);
+});
+
+const schema = gql`
+
+`;
+const resolvers = {
+
+};

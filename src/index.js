de2f73a791caaed22dee44c28a9739d70f1c2656
Britney Smith <brit.smith7@gmail.com>
2019-06-05 14:15:42 -0400

    Extract date fetching logic for code organization

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
-import fetch from 'node-fetch';
-import { find } from 'lodash';
-import { data } from './data.js';
+import { getAllCharacters, getCharacterById } from './api/comic-characters-api';
 
 const app = express();
 const ENV_PORT = process.env.ENV_PORT;
-const allCharactersEndpoint = "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json";
 
 app.use(cors());
 
@@ -35,23 +32,14 @@ const schema = gql`
         character(id: Int!): Character
     }
 `;
-function getData (res) {
-    if (res.status >= 200 && res.status < 300) {
-        return res.json();
-    } else {
-        return data;
-    }
-}
+
 const resolvers = {
     Query: {
         characters: async () => {
-            return fetch(allCharactersEndpoint)
-            .then(getData);
+            return getAllCharacters();
         },
         character: async (_, { id }) => {
-            return fetch(allCharactersEndpoint)
-            .then(getData)
-            .then(characters => find(characters, { "id": id }));
+            return getCharacterById(id);
         }
     },
     Character: {
